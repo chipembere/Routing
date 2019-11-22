4635e4bc7c0c8e7bc1af74b69e41c85fa7d480d6
chipembere <chitiyobrian@gmail.com>
2019-11-22 11:23:54 +0000

    Modified the port in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express'); //$ npm install express -save
 const app = express();
 const bodyParser = require('body-parser')
 // const urlencodedParser = bodyParser.urlencoded({ extended: true })
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(express.static('public'));
 app.use(express.static('views'));
 
